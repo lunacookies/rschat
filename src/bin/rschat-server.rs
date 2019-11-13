a68546ef85ac98f2efcf8590f60debfb693eb2f3
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-11-13 21:18:43 +1100

    [server] Use async-std to handle communications async

--- src/bin/rschat-server.rs
+++ src/bin/rschat-server.rs
@@ -1,42 +1,79 @@
 use anyhow::Result;
+use async_std::{net::TcpStream, prelude::*};
+use rschat::{Sender, Viewer};
 use std::net::SocketAddr;
 
-fn main() -> Result<()> {
+#[async_std::main]
+async fn main() -> Result<()> {
     simple_logger::init()?;
 
     let server_socket = rschat::parse_args()?;
-    listen_for_msgs(server_socket)?;
+    listen_for_msgs(server_socket).await?;
 
     Ok(())
 }
 
-fn listen_for_msgs(socket: SocketAddr) -> Result<()> {
-    use log::{debug, warn};
-    use rschat::Communication;
-    use std::io::Read;
-    use std::net::TcpListener;
+async fn listen_for_msgs(socket: SocketAddr) -> Result<()> {
+    use async_std::{
+        net::TcpListener,
+        sync::{Arc, Mutex},
+        task,
+    };
 
-    let mut viewers = Vec::new();
-    let mut senders = Vec::new();
-    let listener = TcpListener::bind(socket)?;
+    let viewers = Arc::new(Mutex::new(Vec::new()));
+    let senders = Arc::new(Mutex::new(Vec::new()));
 
-    for stream in listener.incoming() {
-        debug!("Received connection");
-        let mut stream = stream?;
+    let listener = TcpListener::bind(socket).await?;
+    let mut incoming = listener.incoming();
 
-        let mut msg = Vec::new();
-        stream.read_to_end(&mut msg)?;
+    let mut tasks = Vec::new();
 
-        let communication: Communication = match rmps::from_read_ref(&msg) {
-            Ok(msg) => msg,
-            Err(e) => {
-                warn!("Failed to deserialise message: {}", e);
-                continue;
-            }
-        };
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
 
-        communication.handle(&mut viewers, &mut senders)?;
+        let viewers = viewers.clone();
+        let senders = senders.clone();
+
+        tasks.push(task::spawn(async move {
+            let mut viewers = viewers.lock().await;
+            let mut senders = senders.lock().await;
+
+            match handle_connection(stream, &mut *viewers, &mut *senders).await {
+                Err(e) => eprintln!("{}", e),
+                _ => (),
+            };
+        }));
     }
+
+    for task in tasks {
+        task.await;
+    }
+
+    Ok(())
+}
+
+async fn handle_connection(
+    mut stream: TcpStream,
+    mut viewers: &mut Vec<Viewer>,
+    mut senders: &mut Vec<Sender>,
+) -> Result<()> {
+    use anyhow::anyhow;
+    use log::debug;
+    use rschat::Communication;
+
+    debug!("Received connection");
+
+    let mut msg = Vec::new();
+    stream.read_to_end(&mut msg).await?;
+
+    let communication: Communication = match rmps::from_read_ref(&msg) {
+        Ok(msg) => msg,
+        Err(e) => {
+            return Err(anyhow!("Error deserialising message: {}", e));
+        }
+    };
+
+    communication.handle(&mut viewers, &mut senders)?;
 
     Ok(())
 }
